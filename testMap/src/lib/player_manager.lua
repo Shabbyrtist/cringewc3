70729ad4d8337797ea91b0a8461bd84dfb53e209
shabbyrtist <shabbyrtist@gmail.com>
2026-07-05 22:27:41 +0300

    merge players_manager in vovalox

--- testMap/src/lib/player_manager.lua
+++ testMap/src/lib/player_manager.lua
@@ -1,44 +1,68 @@
-M = {}
-M.players = {}
+PlayerManager = {}
+PlayerManager.players = {}
 
-function M.init(p)
-    local id = GetPlayerId(p)
-    
-    if GetPlayerController(p) ~= MAP_CONTROL_USER or GetPlayerSlotState(p) ~= PLAYER_SLOT_STATE_PLAYING then
-        return
-    end
-
-    M.players[id] = {
-        active = true,
-        chanceExpl = 0,
-        choiceMade = false,
-        points = 0
-    }
-end
-
-function M.onLeave(p)
+function PlayerManager.onLeave(p)
     local id = GetPlayerId(p)
     
     if M[id] then
-        M.players[id] = nil
+        PlayerManager.players[id].IsInGame = false
+        print('Player ' .. p .. 'has leaved')
     end
 end
 
-function M.isActive(p)
-    local data = M.players[GetPlayerId(p)]
-    --ToDo: How to work temporary shutdown? Mb need to change logic 
+function PlayerManager.isActive(p)
+    local data = PlayerManager.players[GetPlayerId(p)]
     return data and data.active
 end
 
-function M.hasChosen(p)
-    local data = M.players[GetPlayerId(p)]
-
+function PlayerManager.hasChosen(p)
+    local data = PlayerManager.players[GetPlayerId(p)]
     return data and data.choiceMade
 end
 
-function M.setChosen(p, choice)
-    M.players[GetPlayerId(p)].choice = choice
+function PlayerManager.setChosen(p, choice)
+    PlayerManager.players[GetPlayerId(p)].choice = choice
 end
 
-function M.increasChance(p, val)
-    M.players[GetPlayerId(p)] += val
+function PlayerManager.increasChance(p, val)
+    PlayerManager.players[GetPlayerId(p)].points = PlayerManager.players[GetPlayerId(p)].points + val
+end
+
+function PlayerManager.init()
+
+    local trig = CreateTrigger()
+
+    TriggerRegisterTimerEvent(trig, 0.01, false)
+    TriggerAddAction(trig, function()
+        for i = 0, GetBJMaxPlayers() - 1 do
+
+            local p = Player(i)
+    
+            if GetPlayerController(p) ~= MAP_CONTROL_USER or GetPlayerSlotState(p) ~= PLAYER_SLOT_STATE_PLAYING then
+                return
+            end
+
+            PlayerManager.players[i] = {
+                chanceExpl = 0,
+                vp = 0,
+                IsInGame = true
+            }
+
+            print(PlayerManager.players[i].IsInGame)
+
+        end
+    end)
+
+    trig = CreateTrigger()
+    for i = 0, GetBJMaxPlayers() - 1 do
+        TriggerRegisterPlayerEvent(trig, p, EVENT_PLAYER_LEAVE)
+    end
+
+    TriggerAddAction(trig, function()
+        local p = GetTriggerPlayer()
+        PlayerManager.onLeave(p)
+    end)
+
+end
+
+PlayerManager.init()
--- testMap/src/main.lua
+++ testMap/src/main.lua
@@ -1,10 +1,7 @@
 require "spells.spell_handler"
 require "spells.test"
-require "triggers.init"
+require "lib.player_manager"
 
 print('main.lua is here')
-
-local PM = player_manager
-InitAllTriggers(pm)
 
 CreateUnit(Player(0), FourCC('H101'), 219.4, -90.4, 293.630)
